b362785760ffc2caf63f2ba0496f7b0e17ae1638
Joseane da Silva Guedes <joseane@Joseanes-MacBook-Air.local>
2022-05-01 19:13:03 -0300

    initial commit

---
+++ template-literals.js
@@ -0,0 +1,12 @@
+let word1 = 'Joseane';
+let word2 = "Guedes"
+let num1 = 2;
+let num2 = 3;
+
+// const fullName = `${num1 + num2} ${word2}`;
+let example = `${word1} ${word2}`
+// console.log(fullName);
+
+console.log(example)
+
+document.getElementById("example").innerText = example;
