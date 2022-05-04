1c1514b13a6c65c870e13957850dafc5a3974d02
Joseane da Silva Guedes <joseane@Joseanes-MacBook-Air.local>
2022-05-04 17:33:17 -0300

    add forOfLoop

---
+++ forOfLoop/challenge.js
@@ -0,0 +1,9 @@
+const students = [
+    {name: "John", city: "New York"},
+    {name: "Peter", city: "Paris"},
+    {name: "Kate", city: "Sidney"},
+]
+
+for ( const student of students) {
+    console.log(student.name + " lives in " + student.city)
+}
---
+++ forOfLoop/index.js
@@ -0,0 +1,30 @@
+// Example 1
+// let incomes = [62000, 67000, 75000];
+// let total = 0;
+
+// for ( const income of incomes) {
+//     total += income;
+//     console.log(income)
+// }
+
+// console.log(total)
+
+
+
+// Example 2
+// let fullName =  "Josie Guedes";
+
+// for ( const char of fullName) {
+//  console.log(char)
+// }
+
+// Example 3
+
+const cars = ["BMW", "VOLVO", "MINI"]
+
+let text = ''
+for (let x of cars) {
+    text += x + '\n'
+}
+
+console.log(text)
