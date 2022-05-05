f0cbf9662bc93422ce5f9ba7d2378ad99a317689
Joseane da Silva Guedes <joseane@Joseanes-MacBook-Air.local>
2022-05-05 17:01:30 -0300

    default parameters

---
+++ Default Params/challenge.js
@@ -0,0 +1,15 @@
+//challenge
+// const foodShopping = ( food ) => {
+//     console.log(`I am going to buy ${food} from the grocery shop`)
+// }
+
+// foodShopping("milk");
+//I am going to buy milk from the grocery shop
+//======//======// 
+
+const eat = ( food = "something") => {
+    console.log(`I am going to buy ${food} from the grocery shop`)
+}
+
+eat();
+//I am going to buy something from the grocery shop
---
+++ Default Params/index.js
@@ -0,0 +1,25 @@
+// example 1
+// function add(x,y) {
+//     return x + y;
+   
+// }
+// console.log(add (100, 200));
+
+// x and y are the parameters
+// 100 and 200 are the arguments
+
+
+//example 2
+
+// const leadSinger = ( artist = "someone") => {
+//     console.log(`${artist} is the lead singer of Cold Play`)
+// }
+// leadSinger();
+//  someone is the lead singer of Cold Play
+
+// example 3
+const leadSinger = ( artist = "someone") => {
+    console.log(`${artist} is the lead singer of Cold Play`)
+}
+leadSinger("Chris Martin");
+//Chris Martin is the lead singer of Cold Play
