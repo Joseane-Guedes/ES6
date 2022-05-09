a81e7d3bd69beeeff3312729a81eb88e8b6e94a1
Joseane da Silva Guedes <joseane@Joseanes-MacBook-Air.local>
2022-05-08 21:28:49 -0300

    add padStart and PadEnd

---
+++ padStartEnd/challenge.js
@@ -0,0 +1,4 @@
+let example = 'YouTube.com/CodingTutorials360';
+
+console.log(example.padStart(500).length);
+console.log(example.padEnd(1));
---
+++ padStartEnd/index.js
@@ -0,0 +1,7 @@
+//let example = 'Dylan';
+let example = 'Joseane'
+//console.log(example.padStart(10, 'a')) => aaaaaDylan
+
+//console.log(example.padStart(10, 'k')) => kkkJoseane
+
+//console.log(example.padEnd (10,'k')) => Joseanekkk
